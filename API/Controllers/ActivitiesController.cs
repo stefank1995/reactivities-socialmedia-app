8c0985d844591bc7f495822d3905e5fe6c729924
Stefan Kolaric <stefan.kolaric4@gmail.com>
2023-07-26 17:12:07 +0200

    Added Create, Details and Edit elements

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -19,8 +19,22 @@ namespace API.Controllers
         [HttpGet("{id}")] //api/activities/fdgdfdgd2
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
-            return Ok();
+            return await Mediator.Send(new Details.Query{Id = id});
         }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity([FromBody]Activity activity)
+        {
+            return Ok(await Mediator.Send(new Create.Command{Activity = activity}));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditActivity(Guid id, Activity activity)
+        {
+            activity.Id = id;
+            return Ok(await Mediator.Send(new Edit.Command{Activity = activity}));
+        }
+
 
     }
 }
