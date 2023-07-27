184dd25020af80ded89c0becc047398339fdbbcc
Stefan Kolaric <stefan.kolaric4@gmail.com>
2023-07-27 17:10:11 +0200

    Added Extension for builder.services

--- API/Program.cs
+++ API/Program.cs
@@ -3,27 +3,16 @@ using Microsoft.EntityFrameworkCore;
 using MediatR;
 using Application.Activities;
 using Application.Core;
-
+using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(options =>
-{
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddCors(options => {
-    options.AddPolicy("CorsPolicy", policy =>{
-        policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
-    });
-});
-builder.Services.AddMediatR(typeof(List.Handler));
-builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+// Custom Extension
+builder.Services.AddApplicationServices(builder.Configuration);
+
 
 var app = builder.Build();
 
