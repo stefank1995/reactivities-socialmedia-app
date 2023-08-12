ae2ecad1ce32d34fd13369dfd649e572bba290f3
Stefan Kolaric <stefan.kolaric4@gmail.com>
2023-08-12 11:28:00 +0200

    Added Selected Activity functionality to frontend

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -3,10 +3,13 @@ import axios from 'axios';
 import { Container, Header, List } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
-  
+  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
+  const [editMode, setEditMode] = useState(false);
+
   useEffect(() => {
     axios.get<Activity[]>('http://localhost:5000/api/activities')
       .then(response => {
@@ -14,19 +17,37 @@ function App() {
       })
   },[])
 
+  function handleSelectActivity(id: string){
+    setSelectedActivity(activities.find(x => x.id === id));
+  }
+
+  function handleCancelSelectActivity() {
+    setSelectedActivity(undefined);
+  }
+
+  function handleFormOpen(id?: string) {
+    id ? handleSelectActivity(id) : handleCancelSelectActivity();
+    setEditMode(true);
+  }
+
+  function handleFormClose(){
+    setEditMode(false);
+  }
+
   return (
     <Fragment>
-      <NavBar />
+      <NavBar openForm={handleFormOpen} />
       <Container style={{marginTop: '7em'}}>
-        <List>
-            {activities.map( activity => (
-              <List.Item key={activity.id}>
-                {activity.title}
-              </List.Item>
-            ))}
-        </List>
+        <ActivityDashboard 
+          activities={activities}
+          selectedActivity = {selectedActivity}
+          selectActivity = {handleSelectActivity}
+          cancelSelectActivity = {handleCancelSelectActivity}
+          editMode = {editMode}
+          openForm = {handleFormOpen}
+          closeForm = {handleFormClose}
+        />
       </Container>
-        
     </Fragment>
   );
 }
