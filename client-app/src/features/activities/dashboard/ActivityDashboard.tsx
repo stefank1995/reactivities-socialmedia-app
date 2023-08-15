0a2f73f55cbe5380beb9a88e3476016582011c07
Stefan Kolaric <stefan.kolaric4@gmail.com>
2023-08-15 12:55:48 +0200

    Added Delete Functionality for Activities frontend

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, List } from 'semantic-ui-react';
+import { Grid } from 'semantic-ui-react';
 import { Activity } from '../../../app/models/activity';
 import ActivityList from './ActivityList';
 import ActivityDetails from '../details/ActivityDetails';
@@ -13,13 +13,18 @@ interface Props {
     editMode: boolean;
     openForm: (id:string) => void;
     closeForm: () => void;
+    createOrEdit: (activity: Activity) => void;
+    deleteActivity: (id:string) => void;
 }
 
-export default function ActivityDashboard({activities,selectedActivity,selectActivity,cancelSelectActivity, editMode, openForm, closeForm}: Props) {
+export default function ActivityDashboard({activities,selectedActivity,selectActivity,cancelSelectActivity, editMode, openForm, closeForm, createOrEdit, deleteActivity}: Props) {
     return (
         <Grid>
             <Grid.Column width='10'>
-                <ActivityList activities={activities} selectActivity={selectActivity} />
+                <ActivityList activities={activities} 
+                selectActivity={selectActivity} 
+                deleteActivity={deleteActivity}
+                />
             </Grid.Column>
             <Grid.Column width='6'>
                 {selectedActivity && !editMode &&
@@ -29,7 +34,7 @@ export default function ActivityDashboard({activities,selectedActivity,selectAct
                     openForm={openForm}
                     />}
                     {editMode &&
-                    <ActivityForm closeForm={closeForm} activity={selectedActivity} />}
+                    <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit} />}
             </Grid.Column>
         </Grid>
     )
