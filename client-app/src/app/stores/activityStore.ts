896492ad41f86a2e760a82b7a04d858e559493a3
Stefan Kolaric <stefan.kolaric4@gmail.com>
2023-08-18 19:45:17 +0200

    Setup Mobx for managing application state

---
+++ client-app/src/app/stores/activityStore.ts
@@ -0,0 +1,16 @@
+import { action, makeObservable, observable } from "mobx";
+
+export default class ActivityStore {
+    title = 'Hello from MobX!';
+
+    constructor () {
+        makeObservable(this, {
+            title: observable,
+            setTitle: action
+        })
+    }
+
+    setTitle = () => {
+        this.title = this.title + '!';
+    }
+}
